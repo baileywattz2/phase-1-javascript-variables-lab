346e510f8d53a5f5716833084a03d4d8821f122f
bailey watkins <bailey.watkins@rocketmail.com>
2021-10-05 16:16:22 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Code your solution in this file!
 
-const companyName = "Scuber"
-
-let mostProfitableNeighborhood = "Chelsea"
-
-let companyCeo = "Susan Smith"
 
 
+const companyName =  'Scuber'
 
+let mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo = 'Susan Smith'
